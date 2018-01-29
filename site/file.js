63e97e44461459cded28bb301231961b4d0ee70f
ej14433 <32359343+ej14433@users.noreply.github.com>
2018-01-29 23:12:25 +0000

    Delete file.js

--- site/file.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-const jsdom = require("jsdom");
-const { JSDOM } = jsdom;
-
-const { window } = new JSDOM ('<!DOCTYPE html><p>Hello, World!</p>');
-
-
-console.log(window.document.querySelector("html").textContent);
