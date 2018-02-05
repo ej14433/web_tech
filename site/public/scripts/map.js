286fb602fe0047cdd0b770ee22d844f70cc2acef
Elis Jones <ej14433@my.bristol.ac.uk>
2018-02-05 13:04:45 +0000

    Transition example, and CSS things

--- site/public/scripts/map.js
+++ site/public/scripts/map.js
@@ -1,14 +1,13 @@
 function initMap() {
-  var uluru = {lat: 52.215293, lng: -4.358254};
+  var uluru = {lat:52.215293, lng:-4.358254};
 
-  var mapProp = {
-    center: uluru,
-    zoom : 17,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom : 16,
+    center: uluru
+  });
+
   var marker = new google.maps.Marker({
     position: uluru,
     map: map
   });
-  var map = new google.maps.Map(document.getElementById("map"), mapProp);
-  }
+}
