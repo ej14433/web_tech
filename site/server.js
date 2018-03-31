5700e8edea09cb0d21c5c5b4283460a3a198faad
Sonny Peng <sp15240@my.bristol.ac.uk>
2018-03-31 20:30:07 +0100

    Basic transition to expressjs

--- site/server.js
+++ site/server.js
@@ -22,175 +22,204 @@ var verbose = true;
 // See http://en.wikipedia.org/wiki/List_of_HTTP_status_codes.
 // Start the server:
 
+const express = require('express')
+const app = express()
+const path = require('path');
+const bodyParser = require('body-parser')
+
 var http = require("http");
 var fs = require("fs");
 var OK = 200, NotFound = 404, BadType = 415, Error = 500;
 var types, banned;
-start();
 
-// Start the http service. Accept only requests from localhost, for security.
-function start() {
-    if (! checkSite()) return;
-    types = defineTypes();
 
-    banned = [];
-    banUpperCase("./public/", "");
-    var service = http.createServer(handle);
-    service.listen(port, "localhost");
-    var address = "http://localhost";
-    if (port != 80) address = address + ":" + port;
-    console.log("Server running at", address);
-}
+//express style
 
-// Check that the site folder and index page exist.
-function checkSite() {
-    var path = "./public";
-    var ok = fs.existsSync(path);
-    if (ok) path = "./public/index.html";
-    if (ok) ok = fs.existsSync(path);
-    if (! ok) console.log("Can't find", path);
-    return ok;
-}
+app.use(bodyParser());
 
-// Serve a request by delivering a file.
-function handle(request, response) {
-    var url = request.url.toLowerCase();
-    var type = findType(url);
-    if (isBanned(url)){                                 //check invalid url
-      return fail(response, NotFound, "URL has been banned");
-    }else if (url.endsWith("/")) {//check
-      if (fs.existsSync(url)){
-        url = url + "index.html";
-        type = findType(url);
-      }else {
-        return fail(response, BadType, "Folder not found");
-      }
-    }else{
-      if (type == null){
-        var testurl = "./public" + url + ".html";
-        var testfolder = "./public" + url + "/";
-        if (fs.existsSync(testurl)) {
-          url = url + ".html";
-          type = findType(url);
-        }else if (fs.existsSync(testfolder)){
-          var checkindex = testfolder + "index.html";
-          if(fs.existsSync(checkindex)){
-            url = url + "/index.html";
-            type = findType(url);
-          }else{
-            return fail(response, BadType, "File or Folder not found");
-          }
-        }else{
-          return fail(response, BadType, "File or Folder not found");
-        }
-      }
-    }
+Guarding for exceptional errors
+app.get("//", function(req, res) {
+   res.sendFile(__dirname + '/public/index.html');
+});
 
-  //  if (isBanned(url)) return fail(response, NotFound, "URL has been banned");
+app.get("/:id", function(req, res) {
+   res.sendFile(__dirname + '/public/index.html');
+});
 
-//    var type = findType(url);
-    //dealing with HTML and XHTML, New and old browser
 
-//    if (type == null) return fail(response, BadType, "File type unsupported");
-    var file = "./public" + url;
-    fs.readFile(file, ready);
-    function ready(err, content) { deliver(response, type, err, content); }
-}
+//load default
+app.use(express.static(path.join(__dirname, 'public')));
 
-// Forbid any resources which shouldn't be delivered to the browser.
-function isBanned(url) {
-    if (banexploit(url)) return true
-    for (var i=0; i<banned.length; i++) {
-        var b = banned[i];
-        if (url.startsWith(b)) return true;
-    }
-    return false;
-}
+app.listen(port, function () {
+    console.log('Dev app listening on port: ' + port);
+});
 
-// Find the content type to respond with, or undefined.
-function findType(url) {
-    var dot = url.lastIndexOf(".");
-    var extension = url.substring(dot + 1);
-    return types[extension];
-}
 
-// Deliver the file that has been read in to the browser.
-function deliver(response, type, err, content) {
-    if (err) return fail(response, NotFound, "File not found");
-    var typeHeader = { "Content-Type": type };
-    response.writeHead(OK, typeHeader);
-    response.write(content);
-    response.end();
-}
-
-// Give a minimal failure response to the browser
-function fail(response, code, text) {
-    var textTypeHeader = { "Content-Type": "text/plain" };
-    response.writeHead(code, textTypeHeader);
-    response.write(text, "utf8");
-    response.end();
-}
-
-// Check a folder for files/subfolders with non-lowercase names.  Add them to
-// the banned list so they don't get delivered, making the site case sensitive,
-// so that it can be moved from Windows to Linux, for example. Synchronous I/O
-// is used because this function is only called during startup.  This avoids
-// expensive file system operations during normal execution.  A file with a
-// non-lowercase name added while the server is running will get delivered, but
-// it will be detected and banned when the server is next restarted.
-function banUpperCase(root, folder) {
-    var folderBit = 1 << 14;
-    var names = fs.readdirSync(root + folder);
-    for (var i=0; i<names.length; i++) {
-        var name = names[i];
-        var file = folder + "/" + name;
-        if (name != name.toLowerCase()) {
-            if (verbose) console.log("Banned:", file);
-            banned.push(file.toLowerCase());
-        }
-        var mode = fs.statSync(root + file).mode;
-        if ((mode & folderBit) == 0) continue;
-        banUpperCase(root, file);
-    }
-}
-
-function banexploit(url){
-  if(url.includes("/.") || url.includes("//") || url.includes(" ")){
-    return true;
-  }
-}
-
-// The most common standard file extensions are supported, and html is
-// delivered as "application/xhtml+xml".  Some common non-standard file
-// extensions are explicitly excluded.  This table is defined using a function
-// rather than just a global variable, because otherwise the table would have
-// to appear before calling start().  NOTE: add entries as needed or, for a more
-// complete list, install the mime module and adapt the list it provides.
-function defineTypes() {
-    var types = {
-        html : "application/xhtml+xml",
-        css  : "text/css",
-        js   : "application/javascript",
-        png  : "image/png",
-        gif  : "image/gif",    // for images copied unchanged
-        jpeg : "image/jpeg",   // for images copied unchanged
-        jpg  : "image/jpeg",   // for images copied unchanged
-        svg  : "image/svg+xml",
-        json : "application/json",
-        pdf  : "application/pdf",
-        txt  : "text/plain",
-        ttf  : "application/x-font-ttf",
-        woff : "application/font-woff",
-        aac  : "audio/aac",
-        mp3  : "audio/mpeg",
-        mp4  : "video/mp4",
-        webm : "video/webm",
-        ico  : "image/x-icon", // just for favicon.ico
-        xhtml: undefined,      // non-standard, use .html
-        htm  : undefined,      // non-standard, use .html
-        rar  : undefined,      // non-standard, platform dependent, use .zip
-        doc  : undefined,      // non-standard, platform dependent, use .pdf
-        docx : undefined,      // non-standard, platform dependent, use .pdf
-    }
-    return types;
-}
+//
+// //node.js style
+// // Start the http service. Accept only requests from localhost, for security.
+// function start() {
+//     if (! checkSite()) return;
+//     types = defineTypes();
+//
+//     banned = [];
+//     banUpperCase("./public/", "");
+//     var service = http.createServer(handle);
+//     service.listen(port, "localhost");
+//     var address = "http://localhost";
+//     if (port != 80) address = address + ":" + port;
+//     console.log("Server running at", address);
+// }
+//
+// // Check that the site folder and index page exist.
+// function checkSite() {
+//     var path = "./public";
+//     var ok = fs.existsSync(path);
+//     if (ok) path = "./public/index.html";
+//     if (ok) ok = fs.existsSync(path);
+//     if (! ok) console.log("Can't find", path);
+//     return ok;
+// }
+//
+// // Serve a request by delivering a file.
+// function handle(request, response) {
+//     var url = request.url.toLowerCase();
+//     var type = findType(url);
+//     if (isBanned(url)){                                 //check invalid url
+//       return fail(response, NotFound, "URL has been banned");
+//     }else if (url.endsWith("/")) {//check
+//       if (fs.existsSync(url)){
+//         url = url + "index.html";
+//         type = findType(url);
+//       }else {
+//         return fail(response, BadType, "Folder not found");
+//       }
+//     }else{
+//       if (type == null){
+//         var testurl = "./public" + url + ".html";
+//         var testfolder = "./public" + url + "/";
+//         if (fs.existsSync(testurl)) {
+//           url = url + ".html";
+//           type = findType(url);
+//         }else if (fs.existsSync(testfolder)){
+//           var checkindex = testfolder + "index.html";
+//           if(fs.existsSync(checkindex)){
+//             url = url + "/index.html";
+//             type = findType(url);
+//           }else{
+//             return fail(response, BadType, "File or Folder not found");
+//           }
+//         }else{
+//           return fail(response, BadType, "File or Folder not found");
+//         }
+//       }
+//     }
+//
+//   //  if (isBanned(url)) return fail(response, NotFound, "URL has been banned");
+//
+// //    var type = findType(url);
+//     //dealing with HTML and XHTML, New and old browser
+//
+// //    if (type == null) return fail(response, BadType, "File type unsupported");
+//     var file = "./public" + url;
+//     fs.readFile(file, ready);
+//     function ready(err, content) { deliver(response, type, err, content); }
+// }
+//
+// // Forbid any resources which shouldn't be delivered to the browser.
+// function isBanned(url) {
+//     if (banexploit(url)) return true
+//     for (var i=0; i<banned.length; i++) {
+//         var b = banned[i];
+//         if (url.startsWith(b)) return true;
+//     }
+//     return false;
+// }
+//
+// // Find the content type to respond with, or undefined.
+// function findType(url) {
+//     var dot = url.lastIndexOf(".");
+//     var extension = url.substring(dot + 1);
+//     return types[extension];
+// }
+//
+// // Deliver the file that has been read in to the browser.
+// function deliver(response, type, err, content) {
+//     if (err) return fail(response, NotFound, "File not found");
+//     var typeHeader = { "Content-Type": type };
+//     response.writeHead(OK, typeHeader);
+//     response.write(content);
+//     response.end();
+// }
+//
+// // Give a minimal failure response to the browser
+// function fail(response, code, text) {
+//     var textTypeHeader = { "Content-Type": "text/plain" };
+//     response.writeHead(code, textTypeHeader);
+//     response.write(text, "utf8");
+//     response.end();
+// }
+//
+// // Check a folder for files/subfolders with non-lowercase names.  Add them to
+// // the banned list so they don't get delivered, making the site case sensitive,
+// // so that it can be moved from Windows to Linux, for example. Synchronous I/O
+// // is used because this function is only called during startup.  This avoids
+// // expensive file system operations during normal execution.  A file with a
+// // non-lowercase name added while the server is running will get delivered, but
+// // it will be detected and banned when the server is next restarted.
+// function banUpperCase(root, folder) {
+//     var folderBit = 1 << 14;
+//     var names = fs.readdirSync(root + folder);
+//     for (var i=0; i<names.length; i++) {
+//         var name = names[i];
+//         var file = folder + "/" + name;
+//         if (name != name.toLowerCase()) {
+//             if (verbose) console.log("Banned:", file);
+//             banned.push(file.toLowerCase());
+//         }
+//         var mode = fs.statSync(root + file).mode;
+//         if ((mode & folderBit) == 0) continue;
+//         banUpperCase(root, file);
+//     }
+// }
+//
+// function banexploit(url){
+//   if(url.includes("/.") || url.includes("//") || url.includes(" ")){
+//     return true;
+//   }
+// }
+//
+// // The most common standard file extensions are supported, and html is
+// // delivered as "application/xhtml+xml".  Some common non-standard file
+// // extensions are explicitly excluded.  This table is defined using a function
+// // rather than just a global variable, because otherwise the table would have
+// // to appear before calling start().  NOTE: add entries as needed or, for a more
+// // complete list, install the mime module and adapt the list it provides.
+// function defineTypes() {
+//     var types = {
+//         html : "application/xhtml+xml",
+//         css  : "text/css",
+//         js   : "application/javascript",
+//         png  : "image/png",
+//         gif  : "image/gif",    // for images copied unchanged
+//         jpeg : "image/jpeg",   // for images copied unchanged
+//         jpg  : "image/jpeg",   // for images copied unchanged
+//         svg  : "image/svg+xml",
+//         json : "application/json",
+//         pdf  : "application/pdf",
+//         txt  : "text/plain",
+//         ttf  : "application/x-font-ttf",
+//         woff : "application/font-woff",
+//         aac  : "audio/aac",
+//         mp3  : "audio/mpeg",
+//         mp4  : "video/mp4",
+//         webm : "video/webm",
+//         ico  : "image/x-icon", // just for favicon.ico
+//         xhtml: undefined,      // non-standard, use .html
+//         htm  : undefined,      // non-standard, use .html
+//         rar  : undefined,      // non-standard, platform dependent, use .zip
+//         doc  : undefined,      // non-standard, platform dependent, use .pdf
+//         docx : undefined,      // non-standard, platform dependent, use .pdf
+//     }
+//     return types;
+// }
